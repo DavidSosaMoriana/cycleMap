f7203c24cdedad819f973fa8f7507913cbd7bc91
DSosa <sosamorianadav@gmail.com>
2024-11-27 16:51:54 +0000

    clean id page

--- app/networks/[id]/page.tsx
+++ app/networks/[id]/page.tsx
@@ -1,9 +1,11 @@
 import { NetworkDetails } from "@/app/pages/NetworkDetails";
 
-export default function NetworkDetailsPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function NetworkDetailsPage({ params }: Props) {
   return <NetworkDetails networkId={params.id} />;
 }
